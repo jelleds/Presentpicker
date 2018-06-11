11a431bf79628611f5b357e03cc2a72a5df32431
jelleds <jelledesadeleer@gmail.com>
2018-06-11 13:32:07 +0200

    adding the players on the screen

--- script.js
+++ script.js
@@ -1,48 +1,57 @@
 
 $(document).ready(function(){
    
+   //de 2 arrays bepalen
     var arr1=["Jelle", "Verbeken", "Lindsay", "Yenthe", "Delphine"];
     var arr2=["Jelle", "Verbeken", "Lindsay", "Yenthe", "Delphine"];
 
    
 
     $("#unicorn").on("click", function(){
-       
+    
+    //variables
+        //zorgen dat de personen random uit de lijst worden gehaald   
         var random1=arr1[Math.floor(Math.random() * arr1.length)];
         var random2=arr2[Math.floor(Math.random() * arr2.length)];
+        // de indexen van deze personen opvragen
         var index1 = arr1.indexOf(random1);
         var index2 = arr2.indexOf(random2);
-        
-        
-        console.log("Persoon 1: " + random1);
-        console.log("index1: " + index1);
-        console.log("Persoon 2: " + random2);
-        console.log("index2: " + index2);
+        //de 2 personen
+        var persoon1=document.getElementById("persoon1");
+        var persoon2=document.getElementById("persoon2");
+        //de lijst van mensen
+        var array1 = document.getElementById("array1");
+        var players = document.getElementById("players");  
 
+    //Begin of FOR loop
         for(var i in arr1){
 
             if(random1===random2){   
                  
                 arr1.splice(index1,0);
-                console.log(arr1);
                 arr2.splice(index2,0);
-                console.log(arr2);
                                  
             }else{
                 if(random1===arr1[i] || random2===arr2[i]){
                     arr1.splice(index1,1);
-                    console.log(arr1);
                     arr2.splice(index2,1);
-                    console.log(arr2);
                 }
+            }
 
-            }//end for loop
+        } //End of FOR loop
 
+    //Printing the random names on the screen
+        persoon1.innerHTML=random1;
+        persoon2.innerHTML=random2;
 
-        } //End of Else statement
-        
         
     });//End of On Click function
-    
-    
+
+    //printing the players on the screen
+        for(var i in arr1){
+            console.log(arr1[i]);
+            players.innerHTML+="<h4>"+ arr1[i] + "</h4>";
+        }
+   
+
 });//End of ready function 
